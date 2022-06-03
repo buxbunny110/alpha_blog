84c946d8f0e66b87cfa95178287a4188c24a0002
Irakli Asatiani <ekeushka@gmail.com>
2022-06-03 12:47:08 +0400

    show validation errorrs, and successfuly creation message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,24 @@
 class ArticlesController < ApplicationController
-  # GET /articles or /articles.json
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1 or /articles/1.json
   def show
     @article = Article.find(params[:id])
   end
 
-  # GET /articles/new
   def new
+    @article = Article.new
   end
-  # POST /articles or /articles.json
+
   def create
     @article = Article.new(params.require(:articles).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to @article
+    else 
+      render 'new'
+    end
   end
 
 end
