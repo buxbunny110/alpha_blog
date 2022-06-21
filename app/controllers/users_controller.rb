e9d4de94697cd1e89e0a32ca420f367090c4fff9
Irakli Asatiani <ekeushka@gmail.com>
2022-06-21 16:18:31 +0400

    build up sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new 
     @user = User.new
   end
+
+  def create
+    @user = User.new(users_params)
+
+    if @user.save
+      flash[:notice] = 'Welcome to the alpha blog, you have successfully signed up'
+      redirect_to articles_path
+    else
+      render 'new'
+    end 
+    
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
