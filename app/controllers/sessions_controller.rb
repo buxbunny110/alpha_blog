098cce8e6b3fd14fc8c2b62dfc1c5ead64f1827e
Irakli Asatiani <ekeushka@gmail.com>
2022-07-05 20:33:19 +0400

    add up to login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email]
+    password = params[:session][:password]
+
+    user = User.find_by(email: email)
+    if user and user.authenticate(password)
+      flash[:notice] = 'Logged in successfully'
+      redirect_to user
+    else
+      flash.now[:alert] = 'Incorrect credentials'
+      render 'new'
+    end
   end
 
   def destroy
