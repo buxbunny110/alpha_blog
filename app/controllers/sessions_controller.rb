6fb07a23d485921797c3c1bc4c2ea8732fb9e764
Irakli Asatiani <ekeushka@gmail.com>
2022-07-12 20:23:27 +0400

    build up sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
 
     user = User.find_by(email: email)
     if user and user.authenticate(password)
+      session[:user_id] = user.id
       flash[:notice] = 'Logged in successfully'
       redirect_to user
     else
@@ -17,5 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
