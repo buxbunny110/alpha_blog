69dbf5d06411e686c650f8d6cfa0a5e00c6db532
Irakli Asatiani <ekeushka@gmail.com>
2022-08-16 15:43:57 +0400

    add category creating view, test category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:new]
+
   def index
   end
 
@@ -9,5 +11,23 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "#{@category.name} category has been saved"
+    else
+      flash[:warning] = "Some error creating category"
+    end 
+    redirect_to new_category_path
+  end
+
+  private 
+  
+  def set_category
+    @category = Category.new 
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
